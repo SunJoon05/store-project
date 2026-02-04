c68d0da1263a4e84250f9366fcc63b019f98efdf
SunJoon05 <alexsantiagoguerracordoba77@gmail.com>
2026-02-03 22:59:45 -0500

    refactor: actualizacion de conexion a la BD con singleton

--- src/main/java/config/database/DBConnection.java
+++ src/main/java/config/database/DBConnection.java
@@ -1,20 +1,26 @@
 package config.database;
 
-// Class to get the connection with the database
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String HOST = "127.0.0.1";
-    private static final String PORT = "3306";
-    private static final String DATABASE = "store-bd";
-    private static final String URL = "jdbc:mysql://" + HOST + ":"+ PORT + "/" + DATABASE;
-    private static final String USER = "root";
-    private static final String PASSWORD = "1234a";
+
+    private static String DATABASE = "store-bd";
+    private static String HOST = "127.0.0.1";
+    private static String PORT = "3306";
+    private static String USER = "root";
+    private static String PASS = "1234a";
+    private static String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
+    private static Connection connection; // static instance apply the singleton pattern
+
+    private DBConnection(){}
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASS);
+        }
+
+        return connection;
     }
 }
